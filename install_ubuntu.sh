8732bd3304e231ae133642fa4bcc5e9a2387a46c
Heron Yang <heron.yang.tw@gmail.com>
2018-04-21 15:45:12 -0700

    Update setup scripts

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,6 +3,16 @@ sudo apt-get update
 sudo apt-get install -y zsh
 sudo apt-get install -y git-core
 
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zgen
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
 # Downloads the dotfiles
 git clone https://github.com/heronyang/dotfiles /tmp/dotfiles/
 cd /tmp/dotfiles/
@@ -11,7 +21,3 @@ cp ${dotfiles[@]} ~/
 
 # Bridge .bashrc to .bash_profile
 echo "~/.bash_profile" >> ~/.bashrc
-
-# Install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
